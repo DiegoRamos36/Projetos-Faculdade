eb4c2ba18e71321755a24aadd408864030b26a1d
DiegoRamos36 <diegoframos36@gmail.com>
2023-06-26 17:08:39 -0300

    Projetos de faculdade incompletos

---
+++ Kalango/script.js
@@ -0,0 +1,6 @@
+const menuLeito = document.querySelector('.menu-leito');
+const menuSemi = document.querySelector('.menu-semi');
+const menuExec = document.querySelector('.menu-exec');
+const menuLogin = document.querySelector('.popup');
+const ofert = document.querySelector('#ofertas');
+const menuLateral = document.querySelector('.menu-sidebar');
